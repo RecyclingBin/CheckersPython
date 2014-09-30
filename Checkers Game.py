1d48040dd92c5b9a85003acdb63e2718f125d0d1
zachpanz88 <zachary@panzarino.com>
2014-09-30 19:30:07 -0400

    Added Board

--- Checkers Game.py
+++ Checkers Game.py
@@ -1,3 +1,12 @@
 from random import randint
 
+board = []
+
+for x in range(8):
+    board.append(["O"] * 8)
+
+def print_board(board):
+    for row in board:
+        print (" ".join(row))
+
 end_game = raw_input ("Press Enter to End the Game")
