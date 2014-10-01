a5ec00300d66db2ab7469daec9e5f833c22d0756
zachpanz88 <zachary@panzarino.com>
2014-09-30 22:46:09 -0400

    Deleted GUI

--- Checkers Game.py
+++ Checkers Game.py
@@ -6,9 +6,3 @@ for x in range(8):
     board.append(["-"] * 8)
 
 #- will be for no team, X for x team, O for o team
-
-def print_board(board):
-    for row in board:
-        print (" ".join(row))
-		
-print_board(board)
